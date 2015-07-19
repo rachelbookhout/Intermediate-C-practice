0f79f186047badb9d1c136c835b7a4b9ea7b0911
rachelbookhout <rbookhou@gmail.com>
2015-07-19 19:09:17 -0400

    first pass at stopwatch. does not find the exception

---
+++ Unit1/Stopwatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Methods
+{
+	public class Stopwatch
+	{
+		// intialize it with bool false
+		private bool started = false;
+		public bool Started 
+		{
+			get 
+			{ 
+				return started;
+			}
+			set 
+			{ 
+				started = value;
+			}
+			}
+
+			public DateTime Start()
+			{
+				DateTime start = DateTime.Now;
+				//do an if/else method -> throws exception if already true
+				if (started == false) 
+				{
+					bool Started = true;
+					return start;
+				}
+				else 
+				{
+					throw new InvalidOperationException ();
+				}
+			}
+			
+			public DateTime Stop()
+				{
+					DateTime stop = DateTime.Now;
+					bool Started = false;
+					return stop;
+				}
+
+			public TimeSpan Span(DateTime start, DateTime stop)
+				{
+					TimeSpan span = start - stop;
+					return span;
+				}
+
+			}
+		}
+
+
+
