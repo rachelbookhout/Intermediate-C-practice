2ca711d8aed74bfb70bc34f328703bd1a559695d
rachelbookhout <rbookhou@gmail.com>
2015-07-26 15:59:17 -0400

    finished week 1's assignment

--- Unit1/Post.cs
+++ Unit1/Post.cs
@@ -1,22 +1,44 @@
 ﻿using System;
 
-namespace Unit1
+namespace Methods
 {
 	public class Post
 	{
-		public Post ()
+		
+		//has properties for title, description, and dateTime it was created
+		//methods - upvote and downvote
+		// need to see current vote value
+
+		public string title { get; set;}
+		public string description {get;set;}
+		private int upVoteCount = 0;
+		private int downVoteCount = 0;
+		private DateTime _created;
+
+		public DateTime Created {
+			get { return _created; }
+			set{ _created = DateTime.Now;}
+		}
+
+		public void UpVote()
 		{
-			//has properties for title, description, and dateTime it was created
-			//methods - upvote and downvote
-			// need to see current vote value
+			upVoteCount += 1;
+		}
 
+		public void DownVote()
+		{
+			downVoteCount += 1;
+		}
 
-
+		public void DisplayVotes()
+		{
+			Console.WriteLine ("Post titled" + this.title + "has an UpVote count of " + upVoteCount + "and a DownVote:" + downVoteCount);
+		}
 
 
 				//should not give the ability to set the Vote property from the outside,
 					
-		}
+
 	}
 }
 
--- Unit1/Program.cs
+++ Unit1/Program.cs
@@ -8,12 +8,19 @@ namespace Methods
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			//first assignment
 			var clock = new Stopwatch();
 			clock.Start ();
 			clock.Stop ();
 			Console.WriteLine ("Duration" + clock.Span ());
 
+			//second assignment
+			var post = new Post();
+			post.title = "The amazing Rachel";
+			post.UpVote();
+			post.UpVote();
+			post.DownVote();
+			post.DisplayVotes();
 		}
 	}
 }
